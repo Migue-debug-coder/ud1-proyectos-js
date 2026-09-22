1ac2b089be61e0a5794ca8e6fc54683d95e3ba01
Migue-debug-coder <migueldiazromero4@gmail.com>
2026-09-22 10:41:49 +0200

    Primeros 3 ejemplos

---
+++ script1.js
@@ -0,0 +1,19 @@
+// Ejemplo 1
+console.log("Hola mundo")
+
+// Ejemplo 2
+var nombre1 = "Miguel";
+console.log(`Hola ${nombre1}`);
+
+// Ejemplo 3
+var nombre = window.prompt("Introduce tu nombre: ");
+console.log("Hola " + nombre);
+
+// Ejemplo 4 
+var edad = window.prompt("Introduce tu edad: ");
+
+if(edad >= 18){
+    console.log("Eres mayor de edad");
+}else{
+    console.log("Eres menor de edad");
+}
